45c55da43e5dbd4d11805f2a136d48fd9077a567
HeviDevi <devon.r.ross@gmail.com>
2024-04-30 12:27:39 -0400

    added the usage and test sections to both the prompt questions and the template literal. Also added dynamically generated README titles so that a new file is made every time the code runs rather than overwriting the previously generated README file.

---
+++ index.js
@@ -0,0 +1,129 @@
+// inquirer is an ES module, not a CommonJS module, so we need to use import() to load it
+import('inquirer')
+    .then(inquirerModule => {  
+
+      const inquirer = inquirerModule.default;
+      const fs = require('fs');
+
+const licenses = ['MIT', 'Apache', 'GPL', 'BSD', 'MPL', 'None'];
+
+//creates an array of objects that are questions for the user to answer when prompted.
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Write a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.?',
+  },
+  {
+    type : 'input',
+    name: 'usage',
+    message: 'Provide instructions and examples for use.',
+  }, 
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'List your collaborators, if any, with links to their GitHub profiles.',
+  },
+  {
+    type: 'list',
+    name:'license',
+    message: 'What kind of license does your project have?',
+    choices: licenses
+  },
+  {
+    type: 'input',
+    name: 'features',
+    message: 'What are the features of your project?',
+  },
+  {
+    type: 'input',
+    name: 'howToContribute',
+    message: 'If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so.',
+  },
+  {
+    type : 'input',
+    name: 'email',
+    message: 'If you would like to make your email available for questions, please provide it:'
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'If you would like to link your Github profile, please enter your username:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.',
+  }
+  
+];
+
+
+inquirer.prompt(questions)
+  .then((response) => {
+  const newREADME = `
+## ${response.title}
+
+## Description
+
+${response.description}
+
+## Table of Contents
+
+- [Features](#features)
+- [Installation](#installation)
+- [Credits](#credits)
+- [License](#license)
+- [How to Contribute](#how-to-contribute)
+
+## Features
+
+${response.features}
+
+## Installation
+
+${response.installation}
+
+## Usage
+
+${response.usage}
+
+## Credits
+
+${response.credits}
+
+## License
+
+${response.license}
+
+## How to Contribute
+
+${response.howToContribute}
+
+## Questions
+
+Email me with questions at:
+${response.email}
+
+or visit my GitHub profile:
+https://github.com/${response.githubUsername}
+
+## Tests
+
+${response.tests}
+  `;
+
+  fs.writeFile(`README${response.title.replace(/\s/g, '')}.md`, newREADME, (err) =>
+    err ? console.log(err) : console.log('Consider It MARKED DOWN!')
+        )});
+      });
